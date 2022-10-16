2489a4470f920dbe4c63ca2ef8e1ac761101ea0b
Chris Stone <chris.stone-ext@cygest.fr>
2022-10-16 18:27:29 +0400

    Add project files.

---
+++ SignalRInConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace signalRInConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+    }
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+         public IConfiguration Configuration { get; }
+
+    }
+
+
+}
